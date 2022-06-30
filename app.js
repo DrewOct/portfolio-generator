1b47d3099cd0237cd5e0053df353450dc87a46e5
Andrew Seymour <andrew.seymour1@gmail.com>
2022-06-30 15:25:35 -0500

    added require function

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
-const printProfileData = profileDataArr => {
-    //This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+// const printProfileData = profileDataArr => {
+//     //This...
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
 
 
-  console.log('================');
+//   console.log('================');
 
-// Is the same is this...
-profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+// // Is the same is this...
+// profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
