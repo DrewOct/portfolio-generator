a2562d20605acc76d9e4f4d627cf1372f8475727
Andrew Seymour <andrew.seymour1@gmail.com>
2022-06-30 18:40:36 -0500

    added questions

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const generatePage = require('./src/page-template.js');
+// const pageHTML = generatePage(name, github);
 
-const profileDataArgs = process.argv.slice(2);
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//   if (err) throw err;
 
-const [name, github] = profileDataArgs;
-
-// const printProfileData = profileDataArr => {
-//     //This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+console.log(inquirer);
+// inquirer
+// .prompt([
+//   {
+//     type: 'input',
+//     name: 'name',
+//     message: 'What is your name?'
 //   }
-
-
-//   console.log('================');
-
-// // Is the same is this...
-// profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw err;
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// ])
+// .then(answers => console.log(answers));
